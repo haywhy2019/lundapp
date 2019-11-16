4bec111d26ee3278c771fdeb2eddf5cec9354f30
flint <olusegun.sola19@gmail.com>
2019-11-16 21:38:48 +0100

    first

---
+++ main.js
@@ -0,0 +1,31 @@
+const shirt = document.getElementById('1').value
+
+const trousers = document.getElementById('2').value
+
+const jacket = document.getElementById('3').value
+
+const others = document.getElementById('4').value
+
+const button = document.getElementById('button')
+
+
+button.addEventListener('click', input);
+
+function input(){
+    console.log(shirt)
+}
+
+
+
+
+
+
+
+//test
+
+// document.getElementById('test1').addEventListener('click', input);
+
+// function input(){
+//     console.log('clicked')
+// }
+
